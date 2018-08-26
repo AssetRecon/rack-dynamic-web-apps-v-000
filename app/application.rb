27d55edb663e34bcb5c2c3697d2eb5a330ae9e4f
Edsel Caprice <caprice@mail.usf.edu>
2018-08-26 14:33:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,17 @@
 class Application
 
   def call(env)
-    resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.finish
+    rsep = Rack::Response.new
+
+    num_1 = Kernal.rand(1..20)
+    num_2 = Kernal.rand(1..20)
+    num_3 = Kernal.rand(1..20)
+
+    rsep.write "#{num_1}\n"
+    rsep.write "#{num_2}\n"
+    rsep.write "#{num_3}\n"
   end
+
+
 
 end
